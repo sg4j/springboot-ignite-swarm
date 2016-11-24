1f2a21cc73cbe2dae0ef15d2cbdf89aab07f95d6
sg4j <sourabh707@yahoo.com>
2016-11-23 19:43:19 -0500

    Add files via upload

---
+++ src/main/java/com/petclinic/SpringbootJpaMavenApplication.java
@@ -0,0 +1,18 @@
+package com.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+
+@SpringBootApplication
+@EnableCaching
+@EnableCircuitBreaker
+public class SpringbootJpaMavenApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Port is "+args[0]);
+		System.setProperty("server.port", args[0]);
+		SpringApplication.run(SpringbootJpaMavenApplication.class, args);
+	}
+}
